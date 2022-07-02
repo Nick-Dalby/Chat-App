c6eb4a0f4bff1faac08d9f93e57f6129e466dcf2
Nick Dalby <nckdlby@gmail.com>
2022-07-02 15:04:39 +0200

    cleanup

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-
 import 'react-native-gesture-handler'
 
 import { NavigationContainer } from '@react-navigation/native'
